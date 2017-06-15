eb5ab6730741a1d7e0788904712709b9709dea95
Sander Voss <sander@rolisa.nl>
2017-06-15 11:45:35 +0200

    Writer and folder structure

--- unique_items.py
+++ unique_items.py
@@ -1,13 +1,18 @@
 import csv
 import operator
+import os
 
 class UniqueItems():
+    in_dir = "input/"
+    out_dir = "output/"
+
     def __init__(self, filename, key):
-        self.filename = filename
+        self.in_path = os.path.join(self.in_dir,filename)
+        self.out_path = os.path.join(self.out_dir,filename)
         self.key = key
 
     def unique_items(self):
-        with open(self.filename, 'rb') as file:
+        with open(self.in_path, 'rb') as file:
             reader = csv.reader(file, dialect='excel')
             row_names = next(reader)  # gets the first line
             country_names = []
@@ -24,7 +29,7 @@ class UniqueItems():
         for items in country_names:
             dict[items] = 0
 
-        with open(self.filename, 'rb') as file:
+        with open(self.in_path, 'rb') as file:
             reader = csv.reader(file, dialect='excel')
             for rows in reader:
                 if rows[index] in country_names:
@@ -43,12 +48,21 @@ class UniqueItems():
         sorted_dict = sorted(dict_perc.items(), key=operator.itemgetter(1),reverse=True)
         return sorted_dict
 
+    def export_csv(self):
+        dict = self.unique_count()
+        with open(self.out_path, "wb") as file:
+            writer = csv.writer(file, dialect='excel')
+            heading = [self.key, "Percentage", "Occurrence"]
+            writer.writerow(heading)
+            for keys, values in dict:
+                write_row = []
+                write_row.append(keys)
+                write_row.append(values['Percentage'])
+                write_row.append(values['Occurrence'])
+                writer.writerow(write_row)
+countries = UniqueItems(filename="customers.csv",
+                        key="country")
+countries.export_csv()
 
-countries = UniqueItems("customers.csv","country")
-states = UniqueItems("customers.csv", "state")
-cities = UniqueItems("customers.csv", "city")
-
-for items in countries.unique_count():
-    print items
 
 
